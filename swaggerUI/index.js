e3136a852f5e617d83b0a99e0e1db613aecd6ce4
Christopher Nyberg <christopher.nyberg@gmail.com>
2018-10-20 01:12:33 -0700

    Behold, swagger bends to my will

--- swaggerUI/index.js
+++ swaggerUI/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
-const pathToSwaggerUi = require("swagger-ui-dist").absolutePath();
+const fs = require("fs");
+const Mustache = require("mustache");
+//const pathToSwaggerUi = require("swagger-ui-dist").absolutePath();
 
 const app = express();
 
+// Instantiate template
+let template;
+
+app.use("/static", express.static("static"));
+
 const PORT = 1337;
 
-app.use(express.static(pathToSwaggerUi));
+//app.use(express.static(pathToSwaggerUi));
+
+app.get("/", (req, res) => {
+  const view = {
+    awesomeUrl: "http://localhost:3000/docs",
+    path: "./static"
+  };
+  const output = Mustache.render(template, view);
+  res.send(output);
+});
 
 app.listen(PORT, () => {
+  template = fs.readFileSync("./templates/index.html", "utf8");
   console.log(`Running on port ${PORT}`);
 });
