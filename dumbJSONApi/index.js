08e8d1eb8a34a0ddcea9655898186aba009576b8
Christopher Nyberg <christopher.nyberg@gmail.com>
2018-10-19 23:33:34 -0700

    Dumb server complete

--- dumbJSONApi/index.js
+++ dumbJSONApi/index.js
@@ -1,19 +1,21 @@
 const express = require("express");
-const R = require("ramda");
 const app = express();
+const fs = require("fs");
 
 // Constants
 const PORT = 3000;
+let docs;
 
 // Routes
 app.get("/hello", (req, res) => {
-  res.json({ status: "hello" });
+  res.json({ greeting: "hello" });
 });
 
 app.get("/docs", (req, res) => {
-  res.json({ todo: "docs" });
+  res.json(docs);
 });
 // Get Express listening!
 app.listen(PORT, () => {
+  docs = JSON.parse(fs.readFileSync("./swagger.json"));
   console.log(`Alive on port ${PORT}`);
 });
