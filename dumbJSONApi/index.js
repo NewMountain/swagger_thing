b6d3bc21508f250bf262fea10e74d6a0798e22ed
Christopher Nyberg <christopher.nyberg@gmail.com>
2018-10-19 22:57:09 -0700

    Added /docs and a console of the port

--- dumbJSONApi/index.js
+++ dumbJSONApi/index.js
@@ -2,10 +2,18 @@ const express = require("express");
 const R = require("ramda");
 const app = express();
 
+// Constants
+const PORT = 3000;
+
 // Routes
 app.get("/hello", (req, res) => {
   res.json({ status: "hello" });
 });
 
+app.get("/docs", (req, res) => {
+  res.json({ todo: "docs" });
+});
 // Get Express listening!
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Alive on port ${PORT}`);
+});
